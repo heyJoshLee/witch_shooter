f548ff3b07c3963739a980bbc04197858242795d
jleewebdev <lee45276@gmail.com>
2015-12-01 18:55:17 +0800

    Moving background tree added

---
+++ Assets/Scripts/BackgroundMover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundMover : MonoBehaviour {
+
+	public float speed = 0.5f;
+	public GameObject tree_1;
+	private bool spawned = false;
+
+	// Use this for initialization
+	void Start () {
+		SpawnBgObject();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.value > 0.99f) {
+			SpawnBgObject();
+		}
+	}
+	
+	void SpawnBgObject () {
+			GameObject tree = Instantiate(tree_1, new Vector3(200f, 1f, 0f), Quaternion.identity) as GameObject;
+			tree.transform.parent = gameObject.transform;		
+	}
+}
---
+++ Assets/Scripts/BackgroundObjectMover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundObjectMover : MonoBehaviour {
+
+	Rigidbody2D rb2d;
+	public float speed = 0.5f;
+	
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Move object when it becomes alive
+		transform.position += Vector3.left * speed;
+	}
+	
+	void OnTriggerEnter2D (Collider2D col) {
+		// Destroy object when it's off the screen
+		Destroy(gameObject);
+	}
+}
