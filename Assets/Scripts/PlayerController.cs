e9aa98562bd56bd1481cb417b418ed115e5716fd
jleewebdev <lee45276@gmail.com>
2015-12-01 18:06:57 +0800

    Added player vertical movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,25 +3,33 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	float ymin = 0;
-	float ymax = 0;
+	float ymin = 0f;
+	float ymax = 0f;
+	float xmin = 0f;
+	float xmax = 0f;
+	
 	Rigidbody2D rb2d;
-	public float speed = 10;
-	public float padding = 0.5f;
+	public float speed = 10f;
+	public float hPadding = 0.5f;
+	public float leftPadding = 0.5f;
+	public float rightPadding = 0.5f;
 
 	void Start () {
 
-	// Shows distance between object and camera	
-	float distance = transform.position.z - Camera.main.transform.position.z;
-	//
-	Vector3 topmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, distance));
-	Vector3 bottommost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
-	ymax = topmost.y - padding;
-	ymin = bottommost.y + padding;
-	rb2d = GetComponent<Rigidbody2D>();
+		// Shows distance between object and camera	
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		// Finds the topmost and bottommost point of the camera to calculate clamps later
+		Vector3 bottomleft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 topright = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, distance));
+		// Keeps the whole character sprite inside the play area
+		ymin = bottomleft.y + hPadding;
+		ymax = topright.y - hPadding;
+		
+		xmin = bottomleft.x + leftPadding;
+		xmax = topright.x - rightPadding;
+		
+		rb2d = GetComponent<Rigidbody2D>();
 	
-	Debug.Log(ymin);
-	Debug.Log(ymax);
 		
 	
 	}
@@ -36,11 +44,17 @@ public class PlayerController : MonoBehaviour {
 			transform.position += Vector3.down * speed * Time.deltaTime;
 		} else if (Input.GetKey(KeyCode.UpArrow)) {
 			transform.position += Vector3.up * speed * Time.deltaTime;
+		} else if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+		}	else if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+		
+		float newx = Mathf.Clamp(transform.position.x, xmin, xmax);
 		
 		float newy = Mathf.Clamp(transform.position.y, ymin, ymax);
 		
-		transform.position = new Vector2(transform.position.x, newy);
+		transform.position = new Vector2(newx, newy);
 	}
 	
 }
---
+++ Assets/Sprites/BackgroundController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+
+	public GameObject tree_1;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	if(Random.value > 0.02f) {
+		Instantiate(tree_1, new Vector2(0f, 0f), Quaternion.identity); 
+	}
+	
+	}
+}
